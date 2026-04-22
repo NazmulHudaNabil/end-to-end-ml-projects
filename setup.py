89965c2d69e1ae9f48dce5d7c6382e270ed38b13
NazmulHudaNabil <nabil648777@gmail.com>
2026-04-22 23:50:28 +0600

    setup file create

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+
+setup(
+    name='car-price-prediction',
+    version='0.1.0',
+    packages=find_packages(),
+    author_email="nabil648777@gmail.com",
+    description="It's a End to End Machine Learning Project for Car Price Prediction",
+)
